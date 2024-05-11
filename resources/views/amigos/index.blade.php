2d52e6537265e2e508fbd975add8f26e434d53f6
TheGzuckert <gustavo.santoszuckert@gmail.com>
2024-05-11 13:00:46 -0300

    feat: new design to home page and aling itens

--- resources/views/amigos/index.blade.php
+++ resources/views/amigos/index.blade.php
@@ -1,35 +1,44 @@
 <x-layout title="Sorteio de amigo secreto" class="justify-content-between text-center">
 
-  <div class="mt-2 mb-2">
+<div class="mt-2 mb-2">
     <label for="email" class="form-label">Buscar Amigo</label>
-    <div class="col-3">
-      <input type="text" name="email" id="email" class="form-control">
+    <div class="row">
+        <div class="col-md-6">
+            <input type="text" name="email" id="email" class="form-control">
+        </div>
+        <div class="col-md-6">
+            <div class="input-group-append d-flex justify-content-end">
+                <a href="/criar" class="btn btn-dark">Adicionar</a>
+                <a href="/sorteio" class="btn btn-dark" style="margin-left: 10px;">Sortear</a>
+            </div>
+        </div>
     </div>
-  </div>
-
-<div class="mt-6">
-  <p>Lista de Amigos no sorteio:</p>
-<ul class="list-group">
-    @foreach ($amigos as $amigo)
-        <li class="list-group-item d-flex justify-content-between ">
-            <span>{{ $amigo->name }}</span>
-            <div>
-                    <a href="/amigos/{{ $amigo->id }}/edit" class="bi bi-pencil me-2"></a>                  
-                      </form>
-                      <form action="/amigos/{{ $amigo->id }}" method="POST" class="d-inline">
-                          @csrf
-                          @method('DELETE')
-                          <button type="submit" class="bi bi-trash btn btn-link"></button>
-                      </form>
-                  </div>
-              </li>
-          @endforeach
-      </ul>
 </div>
-
-  <div class="d-flex justify-content-between mt-5">
-    <a href="/criar" class="btn btn-dark">Adicionar</a>
-    <a href="/sorteio" class="btn btn-dark">Sortear</a>
-  </div>
+<div class="mt-6">
+  <p>Lista de Amigos no sorteio</p>
+<ul class="list-group">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <strong>Nome</strong>
+        <strong>E-mail</strong>
+        <strong>Opções</strong>
+    </li>
+    @foreach ($amigos as $amigo)
+        <li class="list-group-item">
+            <div class="d-flex justify-content-between align-items-center">
+                <span>{{ $amigo->name }}</span>
+                <span>{{ $amigo->email }}</span>
+                <div>
+                    <a href="/amigos/{{ $amigo->id }}/edit" class="bi bi-pencil me-2"></a>                  
+                    <form action="/amigos/{{ $amigo->id }}" method="POST" class="d-inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="bi bi-trash btn btn-link"></button>
+                    </form>
+                </div>
+            </div>
+        </li>
+    @endforeach
+</ul>
+</div>
 
 </x-layout>
